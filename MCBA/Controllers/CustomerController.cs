16e22bdb5ab052873881e05d57025fca3fa459e0
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-01-19 00:27:08 +1100

    Edit Profile + Display Profile Info Functionality

--- MCBA/Controllers/CustomerController.cs
+++ MCBA/Controllers/CustomerController.cs
@@ -12,22 +12,93 @@ public class CustomerController : Controller
     private readonly MCBAContext _context;
 
     // ReSharper disable once PossibleInvalidOperationException
-    private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+    private int? CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID));
 
     public CustomerController(MCBAContext context)
     {
         _context = context;
     }
 
+    private async Task<Customer> GetCustomer()
+    {
+        var customerID = CustomerID;
+
+        if (!customerID.HasValue)
+        {
+            return null;
+        }
+        return await _context.Customers
+                             .Include(x => x.Accounts)
+                             .FirstOrDefaultAsync(x => x.CustomerID == CustomerID);
+    }
+
     public async Task<IActionResult> Index()
     {
-
-        // Eager loading.
-        var customer = await _context.Customers.Include(x => x.Accounts).
-            FirstOrDefaultAsync(x => x.CustomerID == CustomerID);
-
+        var customer = await GetCustomer();
         return View(customer);
     }
+
+
+    public async Task<IActionResult> MyProfile()
+    {
+        var customer = await GetCustomer();
+
+        if (customer == null)
+        {
+            return Redirect("/");
+
+        }
+        return View(customer);
+    }
+
+    // GET: Display the Edit Profile form
+    [HttpGet]
+    public async Task<IActionResult> EditProfile()
+    {
+        var customer = await GetCustomer();
+
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        var model = new EditProfileViewModel
+        {
+            Name = customer.Name,
+            Address = customer.Address,
+            City = customer.City,
+            PostCode = customer.PostCode
+        };
+
+        return View(model);
+    }
+
+    // POST: Process the Edit Profile form submission
+    [HttpPost]
+    public async Task<IActionResult> EditProfile(EditProfileViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var customer = await GetCustomer();
+
+        if (customer != null)
+        {
+            customer.Name = model.Name;
+            customer.Address = model.Address;
+            customer.City = model.City;
+            customer.PostCode = model.PostCode;
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        return NotFound();
+    }
+
+
 
 
 
---
+++ MCBA/Models/EditProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCBA.Models
+{
+    public class EditProfileViewModel
+    {
+        
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [StringLength(4)]
+        public string PostCode { get; set; }
+
+    }
+}
