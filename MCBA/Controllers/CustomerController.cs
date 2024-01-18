51e459fdc552f07660762a4f3aace3323ee7d4d5
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-01-18 18:46:20 +1100

    Displays Logged in user's accounts info

---
+++ MCBA/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Castle.Core.Resource;
+using MCBA.Data;
+using MCBA.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MCBA.Controllers;
+public class CustomerController : Controller
+{
+    private readonly MCBAContext _context;
+
+    // ReSharper disable once PossibleInvalidOperationException
+    private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+
+    public CustomerController(MCBAContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+
+        // Eager loading.
+        var customer = await _context.Customers.Include(x => x.Accounts).
+            FirstOrDefaultAsync(x => x.CustomerID == CustomerID);
+
+        return View(customer);
+    }
+
+
+
+}
--- MCBA/Program.cs
+++ MCBA/Program.cs
@@ -55,6 +55,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Login}/{id?}");
 
 app.Run();
